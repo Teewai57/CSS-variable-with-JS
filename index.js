c1e9ce73a0e495dbd822aeeac2917f087c51bd11
Teewai57 <bobtoyoabasi@gmail.com>
2022-03-05 23:08:19 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//select all inputs on the page
+
+const inputs = document.querySelectorAll('.controls input')
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+    console.log(this.name);
+}
+
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
